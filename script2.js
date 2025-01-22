845ab20f430a6cd7c1f4c024c0553c08b222c253
Ahmed Nadim <ahmed.mahmoud.nadim@gmail.com>
2025-01-22 21:28:23 +0200

    Create Footer in the UserPage page

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 //All variables (document.querySelector)
 const logOutUser = document.querySelector('.buttonLogout');
-const hiUser = document.querySelector('.loginLeft');
+const hiUser = document.querySelector('.loginLeftUserPage');
 hiUser.textContent = 'Good Morning';
 // All Event Method
 logOutUser.addEventListener('click', (e) => {
