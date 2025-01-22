803fb579d516a80aad8bd46bf7c3cda79fa1c45e
Ahmed Nadim <ahmed.mahmoud.nadim@gmail.com>
2025-01-22 18:06:46 +0200

    Create the Article and the Aside in the UserPage And Finish The UserPage Design and Color

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //All variables (document.querySelector)
 const loginUser = document.querySelector('.buttonLogin');
-
+const hiUser = document.querySelector('.loginLeft');
 // All Event Method
 loginUser.addEventListener('click', (e) => {
   location.href = 'userPage.html';
+  hiUser.textContent = 'Good Morning';
 });
--- script2.js
+++ script2.js
@@ -1,6 +1,7 @@
 //All variables (document.querySelector)
 const logOutUser = document.querySelector('.buttonLogout');
-
+const hiUser = document.querySelector('.loginLeft');
+hiUser.textContent = 'Good Morning';
 // All Event Method
 logOutUser.addEventListener('click', (e) => {
   location.href = 'index.html';
