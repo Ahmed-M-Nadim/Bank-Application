c6681d0b76004dabd77b57d231224b0a83cc5ad4
Ahmed Nadim <ahmed.mahmoud.nadim@gmail.com>
2025-01-23 18:02:45 +0200

    I removed the userPage and make the index page contain hidden userPage and make the hidden false or true whenever I want to appear the userPage or not

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
 //All variables (document.querySelector)
 const loginUser = document.querySelector('.buttonLogin');
+const logoutUser = document.querySelector('.buttonClose');
+const transferUser = document.querySelector('.buttonTransfer');
+const requestUser = document.querySelector('.buttonRequest');
 const hiUser = document.querySelector('.loginLeft');
+const userName = document.querySelector('.userName');
+const userPassword = document.querySelector('.userPassword');
+const userNameOut = document.querySelector('.userNameOut');
+const userPasswordOut = document.querySelector('.userPasswordOut');
+const body = document.querySelector('body');
+const mainPage = document.querySelector('.container');
+const logo = document.querySelector('.logo');
+const mainBalance = document.querySelector('.mainBalance');
+//==================================
+//The Main Object
+const main = [
+  {
+    name: 'Ahmed',
+    password: 1111,
+    balance: 58000,
+    action: [14000, -586, 145, 695, -4587, 6953, 542],
+  },
+  {
+    name: 'Hanem',
+    password: 2222,
+    balance: 34000,
+    action: [13500, 528, -416, -896, 4587, -6953, 568],
+  },
+  {
+    name: 'Nour',
+    password: 3333,
+    balance: 18000,
+    action: [125, 528, -41, -86, 487, -63, -847],
+  },
+];
 // All Event Method
 loginUser.addEventListener('click', (e) => {
-  location.href = 'userPage.html';
-  hiUser.textContent = 'Good Morning';
+  for (i = 0; i < main.length; i++) {
+    if (
+      userName.value == main[i].name &&
+      userPassword.value == main[i].password
+    ) {
+      hiUser.textContent = `Good Morning ${userName.value} `;
+      mainPage.hidden = false;
+      body.classList.add('bodyUserPage');
+      body.classList.remove('bodyIndex');
+      logo.style.width = '80px';
+      mainBalance.textContent = main[i].balance;
+    }
+  }
+});
+
+logoutUser.addEventListener('click', function () {
+  for (i = 0; i < main.length; i++) {
+    if (
+      userNameOut.value == main[i].name &&
+      userPasswordOut.value == main[i].password
+    ) {
+      hiUser.textContent = 'Log in to get started';
+      mainPage.hidden = true;
+    }
+  }
 });
--- script2.js
+++
@@ -1,8 +0,0 @@
-//All variables (document.querySelector)
-const logOutUser = document.querySelector('.buttonLogout');
-const hiUser = document.querySelector('.loginLeftUserPage');
-hiUser.textContent = 'Good Morning';
-// All Event Method
-logOutUser.addEventListener('click', (e) => {
-  location.href = 'index.html';
-});
