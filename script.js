3af758ef2baf6e87b23af6504f1c7a7a940403a6
Ahmed Nadim <ahmed.mahmoud.nadim@gmail.com>
2025-01-24 18:28:19 +0200

    I added My name to the main page and I added the deposit and withdraw amount to the user page with color that appear according to the deposit or the withdraw

--- script.js
+++ script.js
@@ -13,6 +13,35 @@ const body = document.querySelector('body');
 const mainPage = document.querySelector('.container');
 const logo = document.querySelector('.logo');
 const mainBalance = document.querySelector('.mainBalance');
+const balanceDate = document.querySelector('.balanceDate');
+const depWithColor0 = document.querySelector('.depWithColor0');
+const depWithColor1 = document.querySelector('.depWithColor1');
+const depWithColor2 = document.querySelector('.depWithColor2');
+const depWithColor3 = document.querySelector('.depWithColor3');
+const depWithColor4 = document.querySelector('.depWithColor4');
+const depWithColor5 = document.querySelector('.depWithColor5');
+const depWithColor6 = document.querySelector('.depWithColor6');
+const depositWithdrawAmount0 = document.querySelector(
+  '.depositWithdrawAmount0'
+);
+const depositWithdrawAmount1 = document.querySelector(
+  '.depositWithdrawAmount1'
+);
+const depositWithdrawAmount2 = document.querySelector(
+  '.depositWithdrawAmount2'
+);
+const depositWithdrawAmount3 = document.querySelector(
+  '.depositWithdrawAmount3'
+);
+const depositWithdrawAmount4 = document.querySelector(
+  '.depositWithdrawAmount4'
+);
+const depositWithdrawAmount5 = document.querySelector(
+  '.depositWithdrawAmount5'
+);
+const depositWithdrawAmount6 = document.querySelector(
+  '.depositWithdrawAmount6'
+);
 //=============================================================
 //The Main Object
 //*****************
@@ -21,7 +50,7 @@ const main = [
     name: 'Ahmed',
     password: 1111,
     balance: 58000,
-    action: [14000, -586, 145, 695, -4587, 6953, 542],
+    action: [-14000, -586, 145, 695, -4587, 6953, 542],
   },
   {
     name: 'Hanem',
@@ -40,12 +69,32 @@ const main = [
 // All Event Method
 //*******************
 loginUser.addEventListener('click', (e) => {
+  const newArr = [
+    depositWithdrawAmount0,
+    depositWithdrawAmount1,
+    depositWithdrawAmount2,
+    depositWithdrawAmount3,
+    depositWithdrawAmount4,
+    depositWithdrawAmount5,
+    depositWithdrawAmount6,
+  ];
+  const newArr2 = [
+    depWithColor0,
+    depWithColor1,
+    depWithColor2,
+    depWithColor3,
+    depWithColor4,
+    depWithColor5,
+    depWithColor6,
+  ];
+
   for (i = 0; i < main.length; i++) {
     if (
       userName.value == main[i].name &&
       userPassword.value == main[i].password
     ) {
       hiUser.textContent = `Good Morning ${userName.value} `;
+      balanceDate.textContent = Date();
       mainPage.hidden = false;
       body.classList.add('bodyUserPage');
       body.classList.remove('bodyIndex');
@@ -53,7 +102,23 @@ loginUser.addEventListener('click', (e) => {
       mainBalance.textContent = main[i].balance;
       userName.value = '';
       userPassword.value = '';
+      for (j = 0; j < main[i].action.length; j++) {
+        const userAction = main[i].action;
+
+        newArr[j].textContent = userAction[j];
+      }
     }
+  }
+
+  for (z = 0; z < newArr.length; z++) {
+    if (Number(newArr[z].textContent) >= 0) {
+      newArr2[z].textContent = 'Deposit';
+      newArr2[z].style.background = '#322f33';
+    } else if (Number(newArr[z].textContent) < 0) {
+      newArr2[z].textContent = 'Withdraw';
+      newArr2[z].style.background = '#edb257';
+    }
+    console.log(Number(newArr[z].textContent));
   }
 });
 
